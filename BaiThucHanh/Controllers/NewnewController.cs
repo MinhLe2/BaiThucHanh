2cf35766ca094164de20c8c4087b93aa2be9ff24
MinhLe2 <82711269+MinhLe2@users.noreply.github.com>
2021-07-18 22:13:57 +0700

    Initial commit

---
+++ BaiThucHanh/Controllers/NewnewController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiThucHanh.Controllers
+{
+    public class NewnewController : Controller
+    {
+        // GET: Newnew
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public string CheckRadio(String soA, String soB, FormCollection frm)
+        {
+            double a = Convert.ToDouble(soA);
+            double b = Convert.ToDouble(soB);
+            double ketQua = 0;
+            string phepTinh;
+            phepTinh = frm["PhepTinh"].ToString();
+            switch (phepTinh)
+            {
+                case "Cong":
+                    ketQua = a + b;
+                    break;
+                case "Tru":
+                    ketQua = a - b;
+                    break;
+                case "Nhan":
+                    ketQua = a * b;
+                    break;
+                case "Chia":
+                    ketQua = a % b;
+                    break;
+            }
+            return "Ket Qua la:" + ketQua;
+            //if (phepTinh == "Cong")
+            //{
+            //    ketQua = a + b;
+            //    return "Ket Qua la:" + ketQua;
+            //}
+
+            //if (phepTinh == "Tru")
+            //{
+            //    ketQua = a - b;
+            //    return "Ket Qua la:" + ketQua;
+            //}
+
+            //if (phepTinh == "Nhan")
+            //{
+            //    ketQua = a * b;
+            //    return "Ket Qua la:" + ketQua;
+            //}
+
+            //if (phepTinh == "Chia")
+            //{
+            //    ketQua = a % b;
+            //    return "Ket Qua la:" + ketQua;
+            //}
+
+            //ViewData["ketQua"] = ketQua;
+
+        }
+    }
+}
---
+++ BaiThucHanh/Models/Minh.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiThucHanh.Models
+{
+    public class Minh
+    {
+        public int so1  
+        {
+            get;
+            set;
+        }
+        public int so2
+        {
+            get;
+            set;
+        }
+
+    }
+}
